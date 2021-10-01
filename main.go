dae0de4fde936ef47801a92ae44fa1795a788092
yudai <y.sasaki111919@gmail.com>
2021-10-01 17:12:05 +0900

    サーバの起動処理

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    text := "You-saku."
-    fmt.Fprint(w, "Hi there, I'm ", text)
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", nil)
 }
